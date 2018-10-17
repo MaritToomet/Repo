3da9d955e71d3d87fd7d9ed89ee81abb8e61b34d
Liis Talpas <Liis.Talpas@intra.itcollege.ee>
2018-10-17 17:55:25 +0300

    Update Marit_Toomet_var3_NEW.py

--- Marit_Toomet_var3_NEW.py
+++ Marit_Toomet_var3_NEW.py
@@ -2,6 +2,7 @@
 #funktsioon palindrom jagab lause tühikute järgi nö sõnadeks ja kontrollib kas sõna on võrdne sama sõnaga kui ta on ümber pööratud
 def palindrom(argument1):
     tulemus_list= []
+    vastus=[]
     #jagab sõnadeks
     tulemus_list=argument1.split(" ")
     i=0
@@ -9,16 +10,13 @@ def palindrom(argument1):
   
         #kontrollib tingimust (kas sõna= ümber pööratud sõnaga)
         if tulemus_list[i]==tulemus_list[i][::-1]:
-            vastus=[]
             vastus.append(tulemus_list[i])
             i=i+1
-        elif len(vastus) <= 0:
-            print("Polegi palindroome!")
     return vastus
 
 print(palindrom("otto mis teed siis"))
 
-#ÜL Population
+'''#ÜL Population
 # funktsioon leiab naiste ja laste arvu ja liidab need meeste arvuga
 def population(x):
     meeste_arv= x
@@ -29,7 +27,7 @@ def population(x):
     #kogu populatsioon = mehed + naised + lapsed
     vastus= meeste_arv + naiste_arv + laste_arv
     return vastus
-print(population(5))
+print(population(5))'''
 
 
       
